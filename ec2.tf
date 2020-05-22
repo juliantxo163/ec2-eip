4bf19d1aae244563875896fabdb1bf681b259610
julian <juliantxo163@hotmail.com>
2020-05-22 15:50:09 +0200

    user data con http2 añadido

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "modulo" {
   ami           = var.ami_id
   key_name = "virtual-machine"
   instance_type = var.instance_type
+  user_data = file("userdata.txt")
   vpc_security_group_ids  = [
     aws_security_group.sg.id
   ]
